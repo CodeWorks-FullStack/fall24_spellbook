70b5664dab8b0265142064014cb70c3266f4bd95
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-04 12:15:09 -0600

    created spell

--- app/services/SandboxSpellsService.js
+++ app/services/SandboxSpellsService.js
@@ -1,8 +1,10 @@
+import { AppState } from "../AppState.js";
 import { api } from "./AxiosService.js"
 
 class SandboxSpellsService {
   async saveSpell() {
-    const response = await api.post('api/spells')
+    const spellToSave = AppState.activeSpell
+    const response = await api.post('api/spells', spellToSave)
     console.log('SAVED SPELL 🧙‍♂️💾', response.data);
 
   }
